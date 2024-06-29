9a72f8797b1496c8807302f5c0ea79981783e354
HM <hamzafaisaljarral@gmail.com>
2024-06-29 19:33:46 +0400

    fixed for heroku

--- wsgi.py
+++ wsgi.py
@@ -4,5 +4,6 @@ import os
 config_name = os.getenv('FLASK_CONFIG', 'development')
 app = create_app(config_name)
 
-if __name__ != '__main__':
-    gunicorn_app = app
+# This conditional statement is typically used to run the app with Gunicorn
+if __name__ == '__main__':
+    app.run()
